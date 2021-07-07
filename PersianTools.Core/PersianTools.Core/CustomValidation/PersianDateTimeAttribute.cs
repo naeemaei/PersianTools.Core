e1d7b3fdadd5402775c89c24cda8c457f596b4dd
hamed <naeemaei@outlook.com>
2021-07-07 14:25:42 +0430

    Format correction

--- PersianTools.Core/PersianTools.Core/CustomValidation/PersianDateTimeAttribute.cs
+++ PersianTools.Core/PersianTools.Core/CustomValidation/PersianDateTimeAttribute.cs
@@ -3,21 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PersianTools.Core.CustomValidation
 {
-	class PersianDateTimeAttribute : ValidationAttribute
-	{
-		protected PersianDateTimeAttribute()
-		{
-		}
-		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-		{
-			bool result;
-			if (value is PersianDateTime)
-				result = PersianHelper.IsPersianDateValid((PersianDateTime)value);
-			else if (value is String)
-				result = PersianHelper.IsPersianDateValid((string)value);
-			else
-				return new ValidationResult("تاریخ وارد شده معتبر نمی باشد");
-			return result ? ValidationResult.Success : new ValidationResult("تاریخ وارد شده معتبر نمی باشد");
-		}
-	}
+    class PersianDateTimeAttribute : ValidationAttribute
+    {
+        protected PersianDateTimeAttribute()
+        {
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            bool result;
+            if (value is PersianDateTime time)
+                result = PersianHelper.IsPersianDateValid(time);
+            else if (value is String)
+                result = PersianHelper.IsPersianDateValid((string)value);
+            else
+                return new ValidationResult("تاریخ وارد شده معتبر نمی باشد");
+            return result ? ValidationResult.Success : new ValidationResult("تاریخ وارد شده معتبر نمی باشد");
+        }
+    }
 }
--- PersianTools.Core/PersianTools.Core/utility.cs
+++ PersianTools.Core/PersianTools.Core/utility.cs
@@ -4,32 +4,30 @@ using System.Reflection;
 
 namespace PersianTools.Core
 {
-	public static class Utility
-	{
-		/// <summary>
-		/// Get Enum Description
-		/// </summary>
-		/// <param name="value"></param>
-		/// <returns></returns>
-		public static string GetDescription(this Enum value)
-		{
-			Type type = value.GetType();
-			string name = Enum.GetName(type, value);
-			if (name != null)
-			{
-				FieldInfo field = type.GetField(name);
-				if (field != null)
-				{
-					DescriptionAttribute attr =
-						   Attribute.GetCustomAttribute(field,
-							 typeof(DescriptionAttribute)) as DescriptionAttribute;
-					if (attr != null)
-					{
-						return attr.Description;
-					}
-				}
-			}
-			return null;
-		}
-	}
+    public static class Utility
+    {
+        /// <summary>
+        /// Get Enum Description
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name != null)
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    if (Attribute.GetCustomAttribute(field,
+                             typeof(DescriptionAttribute)) is DescriptionAttribute attr)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+            return null;
+        }
+    }
 }
