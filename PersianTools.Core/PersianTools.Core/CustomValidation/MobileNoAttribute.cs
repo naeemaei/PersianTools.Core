284e7ceb63bb12d471de9f778de16caf10b7133e
hamed <naeemaei@outlook.com>
2021-08-26 18:40:07 +0430

    Nothing

--- PersianTools.Core/PersianTools.Core/CustomValidation/MobileNoAttribute.cs
+++ PersianTools.Core/PersianTools.Core/CustomValidation/MobileNoAttribute.cs
@@ -2,16 +2,16 @@
 
 namespace PersianTools.Core.CustomValidation
 {
-	class MobileNoAttribute : ValidationAttribute
-	{
-		protected MobileNoAttribute()
-		{
-		}
-		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-		{
-			if (value.ToString().IsMobileNoValid())
-				return ValidationResult.Success;
-			return new ValidationResult("شماره موبایل وارد شده معتبر نیست");
-		}
-	}
+    class MobileNoAttribute : ValidationAttribute
+    {
+        protected MobileNoAttribute()
+        {
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value.ToString().IsMobileNoValid())
+                return ValidationResult.Success;
+            return new ValidationResult("شماره موبایل وارد شده معتبر نمی باشد");
+        }
+    }
 }
