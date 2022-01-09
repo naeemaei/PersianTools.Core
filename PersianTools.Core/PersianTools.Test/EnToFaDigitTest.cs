818186c800263de360457ae10fcf3f8ed7b045ea
Hamed Naeemaei <h.naeemaei@asanpardakht.ir>
2022-01-09 15:53:07 +0330

    Number conversion documentation

--- PersianTools.Core/PersianTools.Test/EnToFaDigitTest.cs
+++ PersianTools.Core/PersianTools.Test/EnToFaDigitTest.cs
@@ -2,19 +2,19 @@
 
 namespace PersianTools.Test
 {
-	public class EnToFaDigitTest
-	{
-		public EnToFaDigitTest()
-		{
+    public class EnToFaDigitTest
+    {
+        public EnToFaDigitTest()
+        {
 
-		}
-		[Fact]
-		public void Test1()
-		{
-			int price = 11200000;
-			string FaPrice = "یازده میلیون و دویست هزار".Replace(" ", "");
-			string FaPrice1 = PersianTools.Core.CharacterUtil.Convert(price).Replace(" ", "");
-			Assert.Equal(FaPrice, FaPrice1);
-		}
-	}
+        }
+        [Fact]
+        public void Test1()
+        {
+            int price = 11200000;
+            string faPrice = "یازده میلیون و دویست هزار".Replace(" ", "");
+            string faPrice1 = PersianTools.Core.CharacterUtil.Convert(price).Replace(" ", "");
+            Assert.Equal(faPrice, faPrice1);
+        }
+    }
 }
